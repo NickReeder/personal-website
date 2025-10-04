6db5e49849d410d700cad02fa4d9fa26c6a9be5b
NickReeder <123888815+NickReeder@users.noreply.github.com>
2025-10-03 21:50:37 -0400

    deploy: static site from browser build

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment: {
-  appwriteEndpoint: string;
-  appwriteProjectId: string;
-  appwriteProjectName: string;
-} = {
-  appwriteEndpoint: 'https://nyc.cloud.appwrite.io/v1',
-  appwriteProjectId: '68e073040016972ff279',
-  appwriteProjectName: 'personal-wesbsite'
-};
